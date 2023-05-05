3b181857fdb03a5129f8b6a163735ec2ea55fc39
Bernie <bernie@guzzifuzzi.de>
2023-05-05 20:36:39 +0200

    Provide Github tag in welcome message

---
+++ auto_firmware_version.py
@@ -0,0 +1,15 @@
+# credits to https://community.platformio.org/t/how-to-build-got-revision-into-binary-for-version-output/15380/5
+import subprocess
+
+Import("env")
+
+def get_firmware_specifier_build_flag():
+    ret = subprocess.run(["git", "describe", "--dirty"], stdout=subprocess.PIPE, text=True) #Uses dirty tags
+    build_version = ret.stdout.strip()
+    build_flag = "-D AUTO_VERSION=\\\"" + build_version + "\\\""
+    print ("Firmware Revision: " + build_version)
+    return (build_flag)
+
+env.Append(
+    BUILD_FLAGS=[get_firmware_specifier_build_flag()]
+)
--- lib/welcome/welcome.cpp
+++ lib/welcome/welcome.cpp
@@ -1,4 +1,7 @@
 #include <Arduino.h>
+#ifndef AUTO_VERSION
+#define AUTO_VERSION
+#endif
 
 const char* welcomemsg =
 " ██████╗██╗     ██╗ ██████╗██╗  ██╗     ██████╗██╗      ██████╗  ██████╗██╗  ██╗\r\n\
@@ -8,11 +11,13 @@ const char* welcomemsg =
 ╚██████╗███████╗██║╚██████╗██║  ██╗    ╚██████╗███████╗╚██████╔╝╚██████╗██║  ██╗\r\n\
  ╚═════╝╚══════╝╚═╝ ╚═════╝╚═╝  ╚═╝     ╚═════╝╚══════╝ ╚═════╝  ╚═════╝╚═╝  ╚═\r\n\
  A slave clock implementation for ESP8266 by Bernhard Damian 2023.\r\n\
-    for Johannes and Helene\r\n";
+    for Johannes and Helene                                        ";
 
 void welcome() {
     Serial.println();
     Serial.println();
     Serial.println();
-    Serial.println(welcomemsg);
+    Serial.print(welcomemsg);
+    Serial.println(AUTO_VERSION);
+    Serial.println();
 }
