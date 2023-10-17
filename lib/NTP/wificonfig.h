3cc7e99455149a86b4e9faa0cfb6d34fa487e968
Bernie <bernie@guzzifuzzi.de>
2023-10-17 21:02:16 +0200

    Provide a standard working configuration

--- lib/NTP/wificonfig.h
+++ lib/NTP/wificonfig.h
@@ -1,11 +1,11 @@
 #define Config_1
 
 #ifdef Config_1
-    #define DEFAULTLEVEL DEBUG
+    #define DEFAULTLEVEL INFO
     #define HOUR_HAND_DEFAULT   0
     #define MIN_HAND_DEFAULT    0
-    #define SSID         "my_ssid"
-    #define PASSWD       "my_passwd"
+    #define SSID         "TimeNet"
+    #define PASSWD       ""
     #define NTPSERVER    "de.pool.ntp.org"
     #define NTPINTERVAL  1800
     #define TIMEZONE     1
